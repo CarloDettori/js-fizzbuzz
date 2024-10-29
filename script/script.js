028f65f128fc345f3198bd4500908fbeaade18bb
CarloDettori <dettoric07@gmail.com>
2024-10-29 17:15:02 +0100

    condition for Buzz

--- script/script.js
+++ script/script.js
@@ -5,5 +5,8 @@ for(let step=1; step<=100; step=step+1) {
     if (step % 3 == 0) {
         console.log("Fizz")
     }
+    if (step % 5 == 0) {
+        console.log("Buzz")
+    }
         console.log(step)
 }
