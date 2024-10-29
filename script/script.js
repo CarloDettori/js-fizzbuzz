4cad8e363425644f57b3f75ad5a1c8b429547072
CarloDettori <dettoric07@gmail.com>
2024-10-29 17:26:25 +0100

    condition for fizbuzz

--- script/script.js
+++ script/script.js
@@ -2,11 +2,14 @@
 console.clear();
 
 for(let step=1; step<=100; step=step+1) {
-    if (step % 3 == 0) {
-        console.log("Fizz")
-    }
-    if (step % 5 == 0) {
+    if (step % 3 == 0 && step % 5 == 0) {
+        console.log("FizzBuzz")
+    } else if (step % 5 == 0) {
         console.log("Buzz")
-    }
+    } else if (step % 3 == 0) {
+        console.log("Fizz")
+    } else {
         console.log(step)
+    }
+        
 }
