d9e159f29ce51d84b2432230f6ce5433cbec5728
CarloDettori <dettoric07@gmail.com>
2024-10-29 17:33:23 +0100

    comments

--- script/script.js
+++ script/script.js
@@ -1,15 +1,23 @@
+// reset
 "use strict"
 console.clear();
 
+// riga per definire il ciclo da 1 a 100 con incremento di 1 per ogni step
 for(let step=1; step<=100; step=step+1) {
+    //condizione "m.c.m di 5 e 3" per loggare FizzBuzz in console
     if (step % 3 == 0 && step % 5 == 0) {
         console.log("FizzBuzz")
-    } else if (step % 5 == 0) {
-        console.log("Buzz")
-    } else if (step % 3 == 0) {
+    } 
+    //condizione "multiplo di 3" per loggare Fizz in console 
+    else if (step % 3 == 0) {
         console.log("Fizz")
-    } else {
+    } 
+    //condizione "multiplo di 5" per loggare Buzz in console
+    else if (step % 5 == 0) {
+        console.log("Buzz")
+    } 
+    //istruzione per loggare in console il numero del ciclo se nessuna condizione viene soddista 
+    else {
         console.log(step)
     }
-        
 }
